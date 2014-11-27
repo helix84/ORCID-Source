b8a3b26c4b9d61b5087e3cc7b44097cf0c707f11
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-11-27 15:18:16 -0600

    Leaving just one integration test

--- orcid-integration-test/src/test/java/org/orcid/integration/api/t2/test/PopulateOAuthSignInCodeIntegrationTest.java
+++ orcid-integration-test/src/test/java/org/orcid/integration/api/t2/test/PopulateOAuthSignInCodeIntegrationTest.java
@@ -97,18 +97,7 @@ public class PopulateOAuthSignInCodeIntegrationTest extends DBUnitTest {
     String getBaseUrl() {
         return String.format("%s/oauth/authorize?client_id=%s&response_type=code&scope=%s&redirect_uri=%s", webBaseUrl, CLIENT_DETAILS_ID, "/orcid-bio/read-limited",
                 redirectUri);
-    }
-
-    @Test
-    public void checkNoPrePop() throws JSONException, InterruptedException {
-        webDriver.get(getBaseUrl());
-        // make sure we are on the page
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='email']")).getAttribute("value").equals(""));
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='familyNames']")).getAttribute("value").equals(""));
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='givenNames']")).getAttribute("value").equals(""));
-        // verify we don't populate signin
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals(""));
-    }
+    }    
 
     @Test
     public void emailPrePopulate() throws JSONException, InterruptedException {
@@ -132,75 +121,5 @@ public class PopulateOAuthSignInCodeIntegrationTest extends DBUnitTest {
         url = getBaseUrl() + "&email=spike@milligan.com&family_names=test_family_names&given_names=test_given_name&orcid=4444-4444-4444-4441";
         webDriver.get(url);
         assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals("4444-4444-4444-4441"));
-    }
-        
-    @Test
-    public void emailPrePopulateWithHtmlEncodedEmail() throws JSONException, InterruptedException {
-        // test populating form with email that doesn't exist
-        String url = getBaseUrl() + "&email=non_existent%40test.com&family_names=test_family_names&given_names=test_given_name";
-        webDriver.get(url);        
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='email']")).getAttribute("value").equals("non_existent@test.com"));
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='familyNames']")).getAttribute("value").equals("test_family_names"));
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='givenNames']")).getAttribute("value").equals("test_given_name"));
-        // verify we don't populate signin
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals(""));
-
-        // test existing email
-        url = getBaseUrl() + "&email=spike%40milligan.com&family_names=test_family_names&given_names=test_given_name";
-        webDriver.get(url);
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals("spike@milligan.com"));
-        // make sure register
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='email']")).getAttribute("value").equals(""));
-
-        // populating check populating orcid
-        url = getBaseUrl() + "&email=spike%40milligan.com&family_names=test_family_names&given_names=test_given_name&orcid=4444-4444-4444-4441";
-        webDriver.get(url);
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals("4444-4444-4444-4441"));
-    }       
-
-    @Test
-    public void orcidIdPrePopulate() throws JSONException, InterruptedException {
-        // populating check populating orcid
-        String url = getBaseUrl() + "&orcid=4444-4444-4444-4441";
-        webDriver.get(url);
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals("4444-4444-4444-4441"));
-
-        // populating check populating orcid overwrites populating email
-        webDriver.get(getBaseUrl() + "&email=spike@milligan.com&family_names=test_family_names&given_names=test_given_name&orcid=4444-4444-4444-4441");
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals("4444-4444-4444-4441"));
-    }
-
-    @Test
-    public void orcidIdPreopulateWithHtmlEncodedOrcid() throws JSONException, InterruptedException {
-        // populating check populating orcid
-        String url = getBaseUrl() + "&orcid=4444%2D4444%2D4444%2D4441";        
-        webDriver.get(url);
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals("4444-4444-4444-4441"));
-
-        // populating check populating orcid overwrites populating email
-        webDriver.get(getBaseUrl() + "&email=spike@milligan.com&family_names=test_family_names&given_names=test_given_name&orcid=4444%2D4444%2D4444%2D4441");
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals("4444-4444-4444-4441"));
-    }
-    
-    @Test
-    public void givenAndFamilyNamesPrepopulate() throws JSONException, InterruptedException {
-        // test populating form family and given names
-        String url = getBaseUrl() + "&family_names=test_family_names&given_names=test_given_name";
-        webDriver.get(url);
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='familyNames']")).getAttribute("value").equals("test_family_names"));
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='givenNames']")).getAttribute("value").equals("test_given_name"));
-        // verify we don't populate signin
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='userId']")).getAttribute("value").equals(""));
-
-        // test populating form with family name
-        url = getBaseUrl() + "&family_names=test_family_names";
-        webDriver.get(url);
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='familyNames']")).getAttribute("value").equals("test_family_names"));
-
-        // test populating form with given name
-        url = getBaseUrl() + "&given_names=test_given_names";
-        webDriver.get(url);
-        assertTrue(webDriver.findElement(By.xpath("//input[@name='givenNames']")).getAttribute("value").equals("test_given_names"));
-    }
-
+    }        
 }
